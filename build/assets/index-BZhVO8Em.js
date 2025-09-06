e6295d08c769142c639e4ef832a3c3820139e6cd
HighervGround <wcguyton98@gmail.com>
2025-09-06 00:07:17 -0400

    refactor: remove compiled assets from build directory

--- build/assets/index-BZhVO8Em.js
+++
@@ -1,2 +0,0 @@
-import{r as e}from"./index-zpPBrtBy.js";function c(r){const t=e.useRef(r);return e.useEffect(()=>{t.current=r}),e.useMemo(()=>(...u)=>t.current?.(...u),[])}export{c as u};
-//# sourceMappingURL=index-BZhVO8Em.js.map

e4ea93d4c78a963c569e360891cc266f2d23653f
HighervGround <wcguyton98@gmail.com>
2025-09-07 18:01:40 -0400

    refactor: remove unused accessibility settings and navigation components from build assets

---
+++ build/assets/index-BZld24fq.js
@@ -0,0 +1,2 @@
+import{r as t}from"./index-CoDBWMvi.js";var a=globalThis?.document?t.useLayoutEffect:()=>{};export{a as u};
+//# sourceMappingURL=index-BZld24fq.js.map
--- build/assets/index-Go0CN-fz.js
+++
@@ -1,2 +0,0 @@
-import{r as t}from"./index-DB6wa_yx.js";var a=globalThis?.document?t.useLayoutEffect:()=>{};export{a as u};
-//# sourceMappingURL=index-Go0CN-fz.js.map

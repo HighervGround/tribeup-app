caeaab639df14e02db464455f1c2c29a5158504f
HighervGround <wcguyton98@gmail.com>
2025-09-18 11:58:06 -0400

    feat: add VITE_ENABLE_MOCK_DATA environment variable flag
    *Context improved by Giga AI using main-overview.md - Development Guidelines section*

--- build/assets/skeleton-DE2be-gs.js
+++
@@ -1,2 +0,0 @@
-import{j as r}from"./ui-UCUxpkFH.js";import{H as n,I as s}from"./index-B8K4da9u.js";function o(...e){return n(s(e))}function u({className:e,...t}){return r.jsx("div",{className:o("animate-pulse rounded-md bg-muted",e),...t})}export{u as S};
-//# sourceMappingURL=skeleton-DE2be-gs.js.map
